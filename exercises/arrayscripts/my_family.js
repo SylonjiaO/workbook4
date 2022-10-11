6cc0918d76bd27900304144434ea9cc7f7cc5966
Sylonjia Odion <Sodion@atl.yearup.org>
2022-10-11 16:19:25 -0400

    array exercises

---
+++ exercises/arrayscripts/my_family.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let myFamily = ["Traci", "Sylonjia", "Zion", "..." ];
+// for loop
+for (let i = 0; i < myFamily.length; i++) {
+    const famElement = (myFamily[i]);
+    console.log(famElement);
+}
+
+/**while loop**
+let loop = 0;
+while ( loop <= myFamily.length ) {
+    const famElement = myFamily[loop];
+    console.log(famElement);
+  loop++; }
+  */
--- practice/array.js
+++ practice/array.js
@@ -1,7 +1,8 @@
-//declare array
+//create data
 let kids = ["Natalie", "Brittany", "Zachary"];
 //find data
-let fstBorn = kids[1];
-//show data
-console.log(fstBorn);
+for (let i = 0; i < kids.length; i++) {
+    const element = kids[i];
+    console.log(element);}
 
+    //This formula makes sure that if more data is added, the loop just cycles through with jams as apposed to hard coding.
