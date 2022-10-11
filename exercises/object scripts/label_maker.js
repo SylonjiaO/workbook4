f1d338d396a6826c0f0a7df9191fc6000f919d97
Sylonjia Odion <Sodion@atl.yearup.org>
2022-10-11 10:00:18 -0400

    more function work

--- exercises/object scripts/label_maker.js
+++ exercises/object scripts/label_maker.js
@@ -1,9 +1,14 @@
  function printMyInfo(myInfo) {
     console.log("My Name: " + myInfo.name)
-        console.log("My Address: " + myInfo.address) }
-
+        console.log("My Address: " + myInfo.address) 
+   // console.log("My City: " + myInfo.city + " ," + myInfo.state + " " + myInfo.zip); }
+console.log(`My City: ${myInfo.city}, ${myInfo.state}, ${myInfo.zip} `) }
         let myInfo = { name: "Sylonjia Odion",
 address: "123 Main Street",
+city: "San Francisco",
+state: "Ga",
+zip: "12345",
+
 
  };
 
