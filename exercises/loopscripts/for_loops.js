e60da98e01cd89331c61907403e81402d3be7a00
Sylonjia Odion <Sodion@atl.yearup.org>
2022-10-11 14:52:24 -0400

    loops exercise 1

---
+++ exercises/loopscripts/for_loops.js
@@ -0,0 +1,6 @@
+// loop meat 
+for (let index = 0; index < 8; index++) {
+    //output
+    console.log("I love loops");
+    
+}
--- practice/loops.js
+++ practice/loops.js
@@ -1,7 +1,20 @@
+/* do while loop
 let count = 1;
 
 do {
-    console.log(count);
+    console.log(count)
     count = count + 1;
 }
-while (count <= 10);
+while (count <= 10);*/
+
+/*while loop
+let count = 1;
+while (count <= 10) {
+   console.log(count);
+    count = count + 1;
+}*/
+
+/*for loop
+for ( let counter = 0; counter < 10; counter++ ){
+    console.log(counter);
+}*/
