5c9e72ed5a622305eef5f810ae5a65a6a1d5ca20
Sylonjia Odion <Sodion@atl.yearup.org>
2022-10-11 15:05:06 -0400

    loopty loops exercises done

---
+++ exercises/loopscripts/while_loops.js
@@ -0,0 +1,18 @@
+let loop = 1;
+let result = "i love loops"
+
+while(loop <=7) {
+    console.log(result);
+    loop++;
+
+}
+console.log("----------------------------------------------------------------");
+
+let notLoop = 1;
+
+
+while(notLoop <=7) {
+    console.log("I love loops");
+    notLoop++;
+
+}
